969236b9adfff992fd0cf10d26d06e832c0ec4d9
kwame cody <kwamecody@gmail.com>
2025-11-30 15:29:09 +0000

    fix(deploy): finalize paths and PYTHONPATH for production

--- scripts/start_render.sh
+++ scripts/start_render.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 echo "Collecting static files..."
@@ -8,7 +7,7 @@ python src/manage.py collectstatic --noinput
 echo "Starting deployment script..."
 
 echo "Starting Celery..."
-python -m celery -A src.core.celery worker --loglevel=info &
+python -m celery -A src.core worker --loglevel=info &
 
 echo "Starting Gunicorn..."
 exec python -m gunicorn src.core.wsgi:application --bind 0.0.0.0:$PORT --workers 2
